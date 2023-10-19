69e72c05d97a3e64fc93dbd36a3c02b64d34e84e
hooftly <mhooft@tf-dev.org>
2023-10-19 11:49:00 -0600

    add Hash Function for tokenIds

--- src/nft.sol
+++ src/nft.sol
@@ -9,36 +9,38 @@ import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import {ERC721URIStorage} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 contract NFT is ERC721URIStorage {
-
     
     uint256 private nextTokenId;
 
-    uint256 private cap = 5;
+    uint256 private cap;
 
     uint256 public totalSupply;
 
     string public currentTokenURI;
 
+    bytes32 public constant _MINT = keccak256("_MINT");
 
-    constructor(string memory tokenURI) ERC721("NewNFT", "NFT") {
+    constructor(string memory tokenURI, uint256 initialCap) ERC721("NewNFT", "NFT") {
         currentTokenURI = tokenURI;
+        cap = initialCap;
     }
 
     function mintNFT() public returns (uint256) {
         uint256 tokenId = nextTokenId++;
         _mint(msg.sender, tokenId);
-        
         string memory newID = string.concat(currentTokenURI, Strings.toString(tokenId));
         _setTokenURI(tokenId, newID);
-        
         totalSupply = totalSupply + 1;
         require(totalSupply <= cap,"There is a supply cap");
         return tokenId;
     }
     
     function burnNFT(uint256 tokenId) public {
+        address owner = ownerOf(tokenId);
+        require(msg.sender == owner,"You don't own this NFT bruh");
         _burn(tokenId);
         totalSupply = totalSupply - 1;
+        
     }
 
     function setURI(string memory newURI) public returns (string memory) {
@@ -46,7 +48,11 @@ contract NFT is ERC721URIStorage {
         return currentTokenURI;
     } 
 
-   
+    function hashID(uint256 ID) public pure returns (bytes32) {
+        return keccak256(abi.encodePacked(ID));
+    }
+
+    
 
 
 
