fbd2d8374fa52cfd2259eec426d4db96d1f049d3
hooftly <mhooft@tf-dev.org>
2023-10-23 13:40:53 -0600

    mintNFT
    Actually actually use _safeMint

--- src/nft.sol
+++ src/nft.sol
@@ -31,9 +31,9 @@ contract NFT is ERC721URIStorage, AccessControl {
     function mintNFT() public returns (uint256) {
         require(hasRole(_MINT, msg.sender), "You do not have the required role bruh");
         uint256 tokenId = ++nextTokenId;
-        _mint(msg.sender, tokenId);
         string memory newID = string.concat(currentTokenURI, hextool.toHex(hashUserAddress(tokenId)));
         _setTokenURI(tokenId, newID);
+        _safeMint(msg.sender, tokenId);
         totalSupply = totalSupply + 1;
         require(totalSupply <= cap,"There is a supply cap bruh");
         return tokenId;
